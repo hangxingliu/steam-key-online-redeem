884634614020df531831ee7241a4f965fbf5c616
Sola <dev@sola.love>
2017-11-11 14:08:32 +0800

    Update en.js

--- frontend/src/i18n/en.js
+++ frontend/src/i18n/en.js
@@ -1,46 +1,45 @@
 export let strings = {
-	title: "Steam Key Redeem",
+	title: 'Steam Key Redeem',
 	title_login: 'Steam Login',
 
 	server: 'Server: ',
-	disconnect_with_server: 'Disconnected with server!',	
+	disconnect_with_server: 'Disconnected from server!',
 
 	security_information: 'Security Information',
-	si1_1: '1. Please make sure link of this page is starts with ',
-	si1_2: ' (Avoiding account be stolen by others)',
+	si1_1: '1. Please make sure the link of this page is starts with ',
+	si1_2: ' (Prevent account theft)',
 	si2: [
-		'2. Your steam account information will be removed from server after ',
-		'you closed this page or disconnected with server.'
+		'2. Your steam account\'s information will be removed from server after ',
+		'closing this page or disconnecting from server.'
 	],
-	si3: '3. This is an open-source project. Deploying it on your own server is a best way to redeem.',
-	
+	si3: '3. This is an open-source project. Hosting on your own server is the best way to redeem.',
 
 	login_failed: 'Login Failed!',
 
-	login_missing_username: 'please input your steam username',
-	login_missing_password: 'please input your password',
+	login_missing_username: 'username is required',
+	login_missing_password: 'password is required',
 
 	login_username: 'Steam Username',
 	login_password: 'Password',
-	
+
 	login: 'Login',
-	logining: 'Logining...',
+	logining: 'Logging in...',
 
 	steam_guard: 'Steam Guard',
 	steam_guard_from: 'You can in your steam mobile app or you email',
 	btn_continue: 'Continue',
 
-	"export": 'Export',	
+	'export': 'Export',
 	cancel: 'Cancel',
-	export_file_name: 'Export File Name',	
+	export_file_name: 'Export File Name',
 	export_settings: 'Export Settings',
 	export_asf: 'ASF Format',
 	export_redeemed: 'Redeemed',
 	export_failed: 'Failed',
-	export_waiting: 'Waiting for redeeming',
+	export_waiting: 'Pending',
 	export_results: 'Export results',
 	export_filter: 'Export Filter',
-	
+
 	export_filter_redeemed: 'Redeemed',
 	export_filter_waiting: 'Waiting',
 	export_filter_already: 'Failed: Already Purchased',
@@ -48,10 +47,10 @@ export let strings = {
 	export_filter_country: 'Failed: Restricted Country',
 	export_filter_required: 'Failed: Does Not Own Required App',
 
-	notification: 'Desktop notification after finish, disconnected or rate limited ',
+	notification: 'Desktop notification when finished, disconnected or rate limited ',
 	notification_unsupported: '(Your browser does not support it!)',
 	finish: 'Redeem finished!',
-	
+
 	label_keys: 'Keys:',
 	redeem: 'Redeem',
 	redeem_result: 'Redeem Result',
@@ -64,38 +63,37 @@ export let strings = {
 	table_status: 'Status',
 	table_name: 'Name',
 
-
 	status_unsupported: [
 		'Your browser is unsupported. ',
 		'(recommended browser: Chrome, Firefox, Edge)',
 		'(and please keep your browser up to date)'
 	],
 	status_connecting: 'Connecting to server... ',
-	status_connected: 'Connected, you can login now.',
-	status_loggedin: 'You have been logged in, you can redeem keys now. Username:',
+	status_connected: 'Connected, please login.',
+	status_loggedin: 'You have logged in, you can start redeeming keys now. Username:',
 	status_refresh: 'Please refresh this page.',
 
 	made_by: 'Made by ',
-	based_on_project: 'based on project ',
+	based_on_project: 'base on project ',
 	based_on_project_from: ' from ',
 	license_prefix: 'Code released under the',
 	icon_from: 'Icons from ',
-	framework_on: 'Frontend page based on ',
+	framework_on: 'Frontend page is based on ',
 	theme_from: 'Theme from ',
 	fonts_from_1: 'Web fonts (',
-	fonts_from_2: ') are copied fro',
+	fonts_from_2: ') are copied from',
 	donate_link: 'Donate links: ',
 	donation_description: [
-		'Your donation is power for open-source ',
+		'Your donation is powering open-source ',
 		'and free redeeming server.'
 	],
-	
+
 	'result:RetryLater': ' (Retry later)',
 
 	'result:InvalidPassword': 'Invalid password',
-	'result:TwoFactorCodeMismatch': 'Invalid two factor code',
-	'result:Limited account': 'Limited account could not redeeming',
-	'result:AuthCodeError': 'Auth code is error',
+	'result:TwoFactorCodeMismatch': 'Invalid two factor authentication code',
+	'result:Limited account': 'Limited account can not redeem',
+	'result:AuthCodeError': 'Auth code error',
 
 	'result:Waiting': 'Waiting...',
 	'result:Redeeming': 'Redeeming...',
@@ -107,7 +105,7 @@ export let strings = {
 	'result:DuplicateActivationCode': 'Duplicate Activation Code',
 	'result:BadActivationCode': 'Bad Activation Code',
 	'result:RateLimited': 'Rate Limited',
-	'result:DoesNotOwnRequiredApp': 'Does Not Own Required App',
+	'result:DoesNotOwnRequiredApp': 'Does Not Own the Required App',
 	'result:RestrictedCountry': 'Restricted Country',
 
-};
+}
